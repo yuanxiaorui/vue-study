584f7c1e14afddd1adda83e0b23cea4d2b2b4b55
yuanrui <1162795253@qq.com>
2018-08-30 20:49:01 +0800

    'vue'

---
+++ demos/src/components/base/baseInput2.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <input class="input2" v-bind="$attrs" v-on="$listeners" />
+</div>
+</template>
+<script>
+export default {
+  inheritAttrs: false,
+  computed: {
+    inputListeners: function () {
+      var vm = this
+      return Object.assign({},
+        this.$listeners,
+        {
+          focus: function (event) {
+            vm.$emit('focus', event.target.value)
+          }
+        }
+      )
+    }
+  }
+}
+</script>
